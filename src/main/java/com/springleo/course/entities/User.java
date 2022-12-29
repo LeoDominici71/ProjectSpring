7acec145a01236f55c92fd1ce5ff2dbe06ac75d2
LeoDominici71 <leonardo_requena@outlook.com>
2022-12-29 23:56:13 +0000

    JPA,injection,seeding

--- src/main/java/com/springleo/course/entities/User.java
+++ src/main/java/com/springleo/course/entities/User.java
@@ -2,10 +2,21 @@ package com.springleo.course.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 //serializable serve para que o objeto trafegue na rede
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
---
+++ src/main/java/com/springleo/course/repositories/UserRepositories.java
@@ -0,0 +1,13 @@
+package com.springleo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springleo.course.entities.User;
+
+public interface UserRepositories extends JpaRepository<User,Long>{
+
+	
+	
+	
+	
+}
