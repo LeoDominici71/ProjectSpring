8ffcc5787cd268464db4e4c3e83b415f6b661e45
LeoDominici71 <leonardo_requena@outlook.com>
2022-12-30 17:05:35 +0000

    enum OrderStatus

--- src/main/java/com/springleo/course/config/TestConfig.java
+++ src/main/java/com/springleo/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.springleo.course.entities.Order;
 import com.springleo.course.entities.User;
+import com.springleo.course.entities.enums.OrderStatus;
 import com.springleo.course.repositories.OrderRepositories;
 import com.springleo.course.repositories.UserRepositories;
 //esses dois e para fazer o data seeding
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "988888881", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T09:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),u1,OrderStatus.PAID);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T09:42:10Z"),u2,OrderStatus.WAITING_PAYMENT);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),u1,OrderStatus.WAITING_PAYMENT);
 
 
 		
