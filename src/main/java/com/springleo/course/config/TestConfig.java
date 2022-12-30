1b2f687f3a99349e0bffaefb4029236f4607d29d
LeoDominici71 <leonardo_requena@outlook.com>
2022-12-30 02:44:22 +0000

    Order, instant, iso part2

--- src/main/java/com/springleo/course/config/TestConfig.java
+++ src/main/java/com/springleo/course/config/TestConfig.java
@@ -1,5 +1,7 @@
 package com.springleo.course.config;
 
+import java.lang.reflect.Array;
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.springleo.course.entities.Order;
 import com.springleo.course.entities.User;
+import com.springleo.course.repositories.OrderRepositories;
 import com.springleo.course.repositories.UserRepositories;
 //esses dois e para fazer o data seeding
 @Configuration
@@ -16,13 +20,22 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepositories userRepository;
+	@Autowired
+	private OrderRepositories orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "988888881", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T09:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
+
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 
 	}
 
