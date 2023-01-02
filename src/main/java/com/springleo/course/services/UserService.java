b85343266214d9a2ef1a41f471534392bb543254
LeoDominici71 <leonardo_requena@outlook.com>
2023-01-02 01:31:46 +0000

    excecao update

--- src/main/java/com/springleo/course/services/UserService.java
+++ src/main/java/com/springleo/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.springleo.course.repositories.UserRepositories;
 import com.springleo.course.services.exceptions.DatabaseException;
 import com.springleo.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+			
+		}
 	}
 	
 	public void updateData(User entity, User obj) {
